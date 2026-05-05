77e8e18cafa07d84b668af6296b920c5766f771d
njaymeee <njayzar.r1982@gmail.com>
2026-05-05 17:06:39 +0800

    hia1

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import header from './components/header.jsx'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <header/>
+
+    </>
+  )
+}
+
+export default App
---
+++ src/components/header.jsx
@@ -0,0 +1,20 @@
+
+function Header() {
+    <>
+        <header>
+            <nav>
+                <div className="container">
+                    <img src="/public/Icon.jpg" alt="Logo"/>
+                    <h1>Njay's Home</h1>
+                </div>
+                <ul>
+                    <li><a href="/">Home</a></li>
+                    <li><a href="/about">Playlist</a></li>
+                    <li><a href="/activities">Activities</a></li>
+                </ul>
+            </nav>
+        </header>
+    </>
+}
+
+export default Header
