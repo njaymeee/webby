902587fda6435dd7c2b6df2a85e408b6efd1f2b8
njaymeee <njayzar.r1982@gmail.com>
2026-05-05 17:51:25 +0800

    hi3

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,21 +1,22 @@
+import './header.css'
 
 function Header() {
     return(
-            <>
-        <header>
-            <nav>
-                <div className="container">
-                    <img src="/public/Icon.jpg" alt="Logo"/>
-                    <h1>Njay's Home</h1>
-                </div>
-                <ul>
-                    <li><a href="/">Home</a></li>
-                    <li><a href="/about">Playlist</a></li>
-                    <li><a href="/activities">Activities</a></li>
-                </ul>
-            </nav>
-        </header>
-    </>
+        <>
+            <header className="header">
+                <nav className="nav">
+                    <div className="nav-left">
+                        <img src="/public/Icon.jpg" alt="Logo" className="nav-logo"/>
+                        <h1 className="nav-title">Njay's Home</h1>
+                    </div>
+                    <ul className="nav-links">
+                        <li><a href="/">Home</a></li>
+                        <li><a href="/about">Playlist</a></li>
+                        <li><a href="/activities">Activities</a></li>
+                    </ul>
+                </nav>
+            </header>
+        </>
     )
 }
 
